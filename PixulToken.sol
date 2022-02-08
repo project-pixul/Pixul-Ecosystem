48579f0fe09da3ab931806909499a16f1a062204
Vespas Akihito <vespas.akihito@gmail.com>
2022-02-08 01:14:18 -0400

    Info #1 constable variables initialized, Info #2 ApproveandCallback code removed

--- PixulToken.sol
+++ PixulToken.sol
@@ -45,14 +45,6 @@ interface ERC20Interface {
     function transferFrom(address from, address to, uint tokens) external returns (bool success);
 }
 
-// ----------------------------------------------------------------------------
-// Contract function to receive approval and execute function in one call
-// ----------------------------------------------------------------------------
-interface ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes calldata data) external;
-    function onTransferReceived(address operator, address from, uint256 value, bytes calldata data) external returns (bytes4);
-}
-
 
 // ----------------------------------------------------------------------------
 // Owned contract
@@ -277,16 +269,14 @@ contract Pixul is Owned{
     // from the token owner's account. The spender contract function
     // receiveApproval(...) is then executed
     // ------------------------------------------------------------------------
-    function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
+    function approveAndCall(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, address(this), data);
         return true;
     }
 
-    function transferAndCall(address to, uint256 tokens, bytes memory data) public returns (bool success) {
+    function transferAndCall(address to, uint256 tokens) public returns (bool success) {
         transfer(to, tokens);
-        ApproveAndCallFallBack(to).onTransferReceived(address(this),msg.sender,tokens,data);
         return true;
     }
 
