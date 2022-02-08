7927f28bafd061305001130a1f93b662d8b4dad1
Vespas Akihito <vespas.akihito@gmail.com>
2022-02-08 00:26:56 -0400

    Change comment to 0 value transfers are not allowed as per transfer() function

--- PixulToken.sol
+++ PixulToken.sol
@@ -188,7 +188,7 @@ contract Pixul is Owned{
     // ------------------------------------------------------------------------
     // Transfer the balance from token owner's account to to account
     // - Owner's account must have sufficient balance to transfer
-    // - 0 value transfers are allowed
+    // - 0 value transfers are not allowed
     // ------------------------------------------------------------------------
     function transfer(address to, uint tokens) public returns (bool success) {
         _transfer(msg.sender, to, tokens);
